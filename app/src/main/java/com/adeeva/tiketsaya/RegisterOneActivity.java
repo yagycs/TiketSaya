084e66b55f54c84f417e356e793f2574c616ace2
yagycs <yagybatman@gmail.com>
2020-03-16 17:15:25 +0700

    updated

--- app/src/main/java/com/adeeva/tiketsaya/RegisterOneActivity.java
+++ app/src/main/java/com/adeeva/tiketsaya/RegisterOneActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class RegisterOneActivity extends AppCompatActivity {
 
     LinearLayout btn_back;
+    Button btn_continue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +19,21 @@ public class RegisterOneActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register_one);
 
         btn_back = findViewById(R.id.btn_back);
+        btn_continue = findViewById(R.id.btn_continue);
 
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent backtosignin = new Intent(RegisterOneActivity.this, SignInActivity.class);
                 startActivity(backtosignin);
+            }
+        });
+
+        btn_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotonextregister = new Intent(RegisterOneActivity.this, RegisterTwoActivity.class);
+                startActivity(gotonextregister);
             }
         });
     }
