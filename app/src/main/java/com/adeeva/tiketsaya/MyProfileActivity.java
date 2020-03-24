420a54c7c2c76715cfa22cd9c9dfce58c946296c
yagycs <yagybatman@gmail.com>
2020-03-24 10:58:11 +0700

    updated

--- app/src/main/java/com/adeeva/tiketsaya/MyProfileActivity.java
+++ app/src/main/java/com/adeeva/tiketsaya/MyProfileActivity.java
@@ -2,13 +2,39 @@ package com.adeeva.tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MyProfileActivity extends AppCompatActivity {
+
+    LinearLayout item_my_ticket;
+    Button btn_edit_profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+
+        item_my_ticket = findViewById(R.id.item_my_ticket);
+        btn_edit_profile = findViewById(R.id.btn_edit_profile);
+
+        item_my_ticket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotomyticket = new Intent(MyProfileActivity.this, MyTicketDetailActivity.class);
+                startActivity(gotomyticket);
+            }
+        });
+
+        btn_edit_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotoeditprofile = new Intent(MyProfileActivity.this, EditProfileActivity.class);
+                startActivity(gotoeditprofile);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/adeeva/tiketsaya/MyTicketDetailActivity.java
@@ -0,0 +1,14 @@
+package com.adeeva.tiketsaya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyTicketDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_ticket_detail);
+    }
+}
