d84cfebec6808387ff7299b985b0ce69ca27ce07
yagycs <yagybatman@gmail.com>
2020-03-30 17:13:03 +0700

    updated

--- app/src/main/java/com/adeeva/tiketsaya/RegisterTwoActivity.java
+++ app/src/main/java/com/adeeva/tiketsaya/RegisterTwoActivity.java
@@ -1,17 +1,28 @@
 package com.adeeva.tiketsaya;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+
+import com.squareup.picasso.Picasso;
 
 public class RegisterTwoActivity extends AppCompatActivity {
 
     LinearLayout btn_back;
-    Button btn_continue;
+    Button btn_continue, btn_add_photo;
+    ImageView pic_photo_register_user;
+
+    Uri photo_location;
+    Integer photo_max = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,16 @@ public class RegisterTwoActivity extends AppCompatActivity {
 
         btn_back = findViewById(R.id.btn_back);
         btn_continue = findViewById(R.id.btn_continue);
+
+        btn_add_photo = findViewById(R.id.btn_add_photo);
+        pic_photo_register_user = findViewById(R.id.pic_photo_register_user);
+
+        btn_add_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findPhoto();
+            }
+        });
 
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,5 +57,28 @@ public class RegisterTwoActivity extends AppCompatActivity {
                 startActivity(gotosuccess);
             }
         });
+    }
+
+    String getFileExtension(Uri uri){
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return  mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    public void findPhoto(){
+        Intent pic = new Intent();
+        pic.setType("image/*");
+        pic.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(pic, photo_max);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == photo_max && resultCode == RESULT_OK && data != null && data.getData() != null){
+            photo_location = data.getData();
+            Picasso.with(this).load(photo_location).centerCrop().fit().into(pic_photo_register_user);
+        }
     }
 }
