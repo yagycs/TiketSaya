4081ea212a2f94a6820542e86d248b580a0ab8cb
yagycs <yagybatman@gmail.com>
2020-03-30 14:41:23 +0700

    updated

--- app/src/main/java/com/adeeva/tiketsaya/RegisterOneActivity.java
+++ app/src/main/java/com/adeeva/tiketsaya/RegisterOneActivity.java
@@ -1,17 +1,32 @@
 package com.adeeva.tiketsaya;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RegisterOneActivity extends AppCompatActivity {
 
     LinearLayout btn_back;
     Button btn_continue;
+    EditText username, password, email_address;
+    DatabaseReference reference;
+
+    String USERNAME_KEY = "usernamekey";
+    String username_key = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +35,10 @@ public class RegisterOneActivity extends AppCompatActivity {
 
         btn_back = findViewById(R.id.btn_back);
         btn_continue = findViewById(R.id.btn_continue);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        email_address = findViewById(R.id.email_address);
 
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +51,31 @@ public class RegisterOneActivity extends AppCompatActivity {
         btn_continue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                // menyimpan data kepada local storage
+                SharedPreferences sharedPreferences = getSharedPreferences(USERNAME_KEY, MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(username_key, username.getText().toString());
+                editor.apply();
+
+               //simpan kepada database
+                reference = FirebaseDatabase.getInstance().getReference()
+                        .child("Users").child(username.getText().toString());
+                reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        dataSnapshot.getRef().child("username").setValue(username.getText().toString());
+                        dataSnapshot.getRef().child("password").setValue(password.getText().toString());
+                        dataSnapshot.getRef().child("email_address").setValue(email_address.getText().toString());
+                        dataSnapshot.getRef().child("user_balance").setValue(800);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
                 Intent gotonextregister = new Intent(RegisterOneActivity.this, RegisterTwoActivity.class);
                 startActivity(gotonextregister);
             }
